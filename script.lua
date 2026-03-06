719266ace15735e2a2f0e4ad4061c0a8a00dcf57
alqrnyahmd266-ship-it <alqrnyahmd266@gmail.com>
2026-03-06 10:53:21 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 -- قائمة الحسابات المسموح لها (ضع الأسماء هنا)
-local AllowedUsers = {"", "", "ahmoooood209"} 
+local AllowedUsers = {"", "", "Ahmoooood209"} 
 
 local Player = game:GetService("Players").LocalPlayer
 local isAllowed = false
